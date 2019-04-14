8e3b7afa879a43fcdd2dd1b86d441c51c7b59f54
zhangran <zhangran@42.com>
2019-04-14 11:06:42 +0800

    leetcode 203

--- leetcode/linkedlist/remove_eletemts.py
+++ leetcode/linkedlist/remove_eletemts.py
@@ -30,3 +30,61 @@ class Solution(object):
             return None
         head.next = self.removeElements(head.next, val)
         return head.next if head.val == val else head
+
+
+class Solution1(object):
+    def removeElements(self, head, val):
+        """
+        虚拟头结点
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dummy_head = ListNode(None)
+        dummy_head.next = head
+
+        pre = dummy_head
+        while pre.next:
+            cur = pre.next
+            if cur.val == val:
+                pre.next = cur.next
+                cur.next = None
+            else:
+                pre = pre.next
+
+        return dummy_head.next
+
+
+class Solution2(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head and head.val == val:
+            del_node = head
+            head = head.next
+            del_node.next = None
+
+        if not head:
+            return head
+
+        pre = head
+        while pre.next:
+            cur = pre.next
+            if cur.val == val:
+                pre.next = cur.next
+                cur.next = None
+            else:
+                pre = pre.next
+        return head
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head = Solution1().removeElements(head, 1)
+    print(head)
