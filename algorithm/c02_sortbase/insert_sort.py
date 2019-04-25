0abcf8cdea1caf3ead58bc6cc6ada89e9b291c95
zhangran <zhangran@42.com>
2019-04-25 18:14:36 +0800

    insert sort

--- algorithm/c02_sortbase/insert_sort.py
+++ algorithm/c02_sortbase/insert_sort.py
@@ -13,7 +13,20 @@ def insert_sort(arr):
                 break
 
 
+def insert_sort2(arr):
+    for i in range(1, len(arr)):
+        cur = arr[i]
+        index = i
+        for j in range(i, 0, -1):
+            if arr[j - 1] > cur:
+                arr[j] = arr[j - 1]
+                index = j - 1
+            else:
+                break
+        arr[index] = cur
+
+
 if __name__ == "__main__":
     a = [24, 23, 5, 2, 6, 8, 3, 1, 8]
-    insert_sort(a)
+    insert_sort2(a)
     print(a)
