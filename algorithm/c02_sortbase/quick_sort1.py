a8619b7c8d54a38a3a53a7290ae8b396fbc7b4e5
zhangran <zhangran@42.com>
2019-04-30 14:30:44 +0800

    quick sort 1

---
+++ algorithm/c02_sortbase/quick_sort1.py
@@ -0,0 +1,28 @@
+def quick_sort(arr):
+    _quick_sort(arr, 0, len(arr) - 1)
+
+
+def _quick_sort(arr, l, r):
+    if l >= r:
+        return
+    p = _partition(arr, l, r)
+    _quick_sort(arr, l, p - 1)
+    _quick_sort(arr, p + 1, r)
+
+
+def _partition(arr, l, r):
+    v = arr[l]
+    j = l
+
+    for i in range(l + 1, r + 1):
+        if arr[i] < v:
+            arr[j + 1], arr[i] = arr[i], arr[j + 1]
+            j += 1
+    arr[l], arr[j] = arr[j], arr[l]
+    return j
+
+
+if __name__ == '__main__':
+    a = [28, 6, 3, 123, 5, 3, 23, 54, 1, 34]
+    quick_sort(a)
+    print(a)
