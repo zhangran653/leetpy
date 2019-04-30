b15e44578a97498022e5a3b5a21215098337b1fa
zhangran <zhangran@42.com>
2019-04-30 12:52:37 +0800

    merge sort bu

--- practise/allinone.py
+++ practise/allinone.py
@@ -32,11 +32,48 @@ def _merge(arr, l, mid, r):
             j += 1
 
 
+def merge_sort_bu(arr):
+    sz = 1
+    while sz < len(arr):
+        lo = 0
+        while lo + sz - 1 <= len(arr):
+            _merge(arr, lo, lo + sz - 1, min(lo + sz * 2 - 1, len(arr) - 1))
+            lo = lo + sz * 2
+        sz *= 2
+
+
 # -------- merge sort --------
 
+# -------- select sort -------
+def select_sort(arr):
+    for i in range(0, len(arr)):
+        index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[index]:
+                index = j
+        arr[i], arr[index] = arr[index], arr[i]
+
+
+# -------- select sort -------
+
+# ------- insert sort -------
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        min_index = i
+        temp = arr[i]
+        for j in range(i, 0, -1):
+            if temp < arr[j - 1]:
+                arr[j] = arr[j - 1]
+                min_index = j - 1
+            else:
+                break
+        arr[min_index] = temp
+
+
+# ------- insert sort -------
 
 if __name__ == '__main__':
     a = [354, 23, 65, 43, 1, 456, 43, 32, 76, 43, 675, 43, 23]
 
-    merge_sort(a)
+    merge_sort_bu(a)
     print(a)
